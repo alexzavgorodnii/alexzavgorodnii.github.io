a2ca44fe9355aa2011a68549735e64cf3a95909e
Aleksandr <azavgorodniy@outlook.com>
2016-06-19 13:42:20 +0300

    Upload main page
    Main page

---
+++ Gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function() {
+    gulp.src('scss/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('prod/css/'));
+});
+
+gulp.task('default', function() {
+  gulp.watch('./scss/**/*.scss', ['sass']);
+});
